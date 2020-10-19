43afd46bec6a847abe194aaae06beb31b61f355a
Jana Häusler <jana.m.haeusler@gmail.com>
2020-10-19 18:57:57 +0200

    Add header component in cop logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navigation from './Navigation'
 import { getDataJsAll } from './js/lib'
+import Header from './Header'
 import './styles/basic.css'
 
 import dashboardIconActive from '/assets/icons/dashboardicon-active.svg'
@@ -17,71 +18,96 @@ import energyIconInactive from '/assets/icons/energyicon-inactive.svg'
 import journalIconActive from '/assets/icons/journalicon-active.svg'
 import journalIconInactive from '/assets/icons/journalicon-inactive.svg'
 
-const routes = [
-  {
-    //index[0]
-    path: '/',
-    //component: Page({ target: el, text: 'Dashboard', hidden: false }),
-    title: 'Dashboard',
-    srcActive: dashboardIconActive,
-    srcInactive: dashboardIconInactive,
-  },
-  {
-    //index[1]
-    path: '/code-buddys',
-    //component: Page({ target: el, text: 'Code Buddys', hidden: true }),
-    title: 'Code Buddys',
-    srcActive: buddyIconActive,
-    srcInactive: buddyIconInactive,
-  },
-  {
-    //index[2]
-    path: '/teams',
-    //component: Page({ target: el, text: 'Teams', hidden: true }),
-    title: 'Teams',
-    srcActive: teamIconActive,
-    srcInactive: teamIconInactive,
-  },
-  {
-    //index[3]
-    path: '/energy',
-    //component: Page({ target: el, text: 'Energy', hidden: true }),
-    title: 'Energy',
-    srcActive: energyIconActive,
-    srcInactive: energyIconInactive,
-  },
-  {
-    //index[4]
-    path: '/journal',
-    //component: Page({ target: el, text: 'journal', hidden: true }),
-    title: 'Journal',
-    srcActive: journalIconActive,
-    srcInactive: journalIconInactive,
-  },
+const headerContent = [{
+        path: '/',
+        headline: 'Dashboard',
+    },
+    {
+        path: '/code-buddys',
+        headline: 'Code Buddys',
+        subheadline: 'on Monday — 18.07.2020',
+    },
+    {
+        path: '/teams',
+        headline: 'Teams',
+        subheadline: 'for Exercise 1',
+    },
+    {
+        path: '/energy',
+        headline: 'Energy',
+    },
+    {
+        path: '/journal',
+        headline: 'Journal',
+    },
+]
+
+Header({ defaultHeadline: 'Dashboard', defaultSubHeadline: 'SUP Headline' })
+
+const routes = [{
+        //index[0]
+        path: '/',
+        //component: Page({ target: el, text: 'Dashboard', hidden: false }),
+        title: 'Dashboard',
+        srcActive: dashboardIconActive,
+        srcInactive: dashboardIconInactive,
+    },
+    {
+        //index[1]
+        path: '/code-buddys',
+        //component: Page({ target: el, text: 'Code Buddys', hidden: true }),
+        title: 'Code Buddys',
+        srcActive: buddyIconActive,
+        srcInactive: buddyIconInactive,
+    },
+    {
+        //index[2]
+        path: '/teams',
+        //component: Page({ target: el, text: 'Teams', hidden: true }),
+        title: 'Teams',
+        srcActive: teamIconActive,
+        srcInactive: teamIconInactive,
+    },
+    {
+        //index[3]
+        path: '/energy',
+        //component: Page({ target: el, text: 'Energy', hidden: true }),
+        title: 'Energy',
+        srcActive: energyIconActive,
+        srcInactive: energyIconInactive,
+    },
+    {
+        //index[4]
+        path: '/journal',
+        //component: Page({ target: el, text: 'journal', hidden: true }),
+        title: 'Journal',
+        srcActive: journalIconActive,
+        srcInactive: journalIconInactive,
+    },
 ]
 
 Navigation({ targetSelector: 'footer', onNavigate: handleNavigate, routes })
 
 function handleNavigate(path) {
-  /*const newRoute = routes.find((route) => route.path === path)
-                header.update(newRoute.title)
-                routes.forEach((route) =>
-                  route === newRoute ? route.component.show() : route.component.hide()
-                )*/
+    /*const newRoute = routes.find((route) => route.path === path)
+                              header.update(newRoute.title)
+                              routes.forEach((route) =>
+                                route === newRoute ? route.component.show() : route.component.hide()
+                              )*/
 
-  const pages = getDataJsAll('page')
-  pages.forEach((page) => {
-    const pageName = page.dataset.name
-    const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
-    page.classList.toggle('d-none', targetName !== pageName)
-  })
+    const pages = getDataJsAll('page')
+    pages.forEach((page) => {
+        const pageName = page.dataset.name
+        const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
+        page.classList.toggle('d-none', targetName !== pageName)
+    })
 
-  const headers = getDataJsAll('header')
-  headers.forEach((header) => {
-    const headerName = header.dataset.name
-    const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
-    header.classList.toggle('d-none', targetName !== headerName)
-  })
+    const headers = getDataJsAll('header')
+    headers.forEach((header) => {
+        const headerName = header.dataset.name
+        const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
+        header.classList.toggle('d-none', targetName !== headerName)
+    })
 
-  //console.log('Grüße von der handleNavigate')
+    //console.log('Grüße von der handleNavigate')
 }
---
+++ src/Header.js
@@ -0,0 +1,74 @@
+import { createElement, createElementPlaceStart } from './js/lib'
+import backgroundImg from './assets/background_shape_1.svg'
+import './Header.css'
+
+export default function Header({
+    defaultHeadline,
+    defaultSubHeadline,
+    update,
+}) {
+    console.log(defaultHeadline, defaultSubHeadline)
+        //const el = createElementPlaceStart({ type: 'header', className: 'header' })
+        //- `element.insertAdjacentHTML(position, text);` <-> fügt etwas an einem bestimmten punkt hinzu (beforebegin , afterbegin, beforeend, afterend)
+
+    document.body.insertAdjacentHTML(
+        'afterbegin',
+        '<header class="header"></header>'
+    )
+
+    const el = document.querySelector('header')
+
+    const imgWave = createElement({ type: 'img', target: el, className: 'header__img' })
+
+    imgWave.src = backgroundImg
+
+    const headline = createElement({
+        type: 'h1',
+        target: el,
+        className: 'header__headline',
+    })
+    headline.textContent = defaultHeadline;
+
+    const subheadline = createElement({
+        type: 'span',
+        target: el,
+        className: 'header__subheadline regular-12',
+    })
+    subheadline.textContent = defaultSubHeadline;
+
+
+    // füllen.
+    function update(newHeadline, newSubheadline = '') {
+        console.log('update!')
+    }
+}
+
+/* <header class="header">
+<h1 class="header__headline bold-16">Journal</h1>
+<p class="header__subheadline regular-12"></p>
+</header> */
+
+/*
+const headerContent = [{
+    path: '/',
+    headline: 'Dashboard',
+},
+{
+    path: '/code-buddys',
+    headline: 'Code Buddys',
+    subheadline: 'on Monday — 18.07.2020',
+},
+{
+    path: '/teams',
+    headline: 'Teams',
+    subheadline: 'for Exercise 1',
+},
+{
+    path: '/energy',
+    headline: 'Energy',
+},
+{
+    path: '/journal',
+    headline: 'Journal',
+}
+]*/
