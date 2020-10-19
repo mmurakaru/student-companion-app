3610053a872277562dcb197f4a85fc77fa84e29d
Jana Häusler <jana.m.haeusler@gmail.com>
2020-10-19 15:43:45 +0200

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navigation from './Navigation'
-import {getDataJsAll} from './js/lib'
+import { getDataJsAll } from './js/lib'
 import './styles/basic.css'
 
 import dashboardIconActive from '/assets/icons/dashboardicon-active.svg'
@@ -17,69 +17,71 @@ import energyIconInactive from '/assets/icons/energyicon-inactive.svg'
 import journalIconActive from '/assets/icons/journalicon-active.svg'
 import journalIconInactive from '/assets/icons/journalicon-inactive.svg'
 
-
 const routes = [
-    {
-      path: '/',
-      //component: Page({ target: el, text: 'Dashboard', hidden: false }),
-      title: 'Dashboard',
-      srcActive: dashboardIconActive,
-      srcInactive: dashboardIconInactive,
-    },
-    {
-      path: '/code-buddys',
-      //component: Page({ target: el, text: 'Code Buddys', hidden: true }),
-      title: 'Code Buddys',
-      srcActive: buddyIconActive,
-      srcInactive: buddyIconInactive,
-    },
-    {
-      path: '/teams',
-      //component: Page({ target: el, text: 'Teams', hidden: true }),
-      title: 'Teams',
-      srcActive: teamIconActive,
-      srcInactive: teamIconInactive,
-    },
-    {
-      path: '/energy',
-      //component: Page({ target: el, text: 'Energy', hidden: true }),
-      title: 'Energy',
-      srcActive: energyIconActive,
-      srcInactive: energyIconInactive,
-    },
-    {
-        path: '/journal',
-        //component: Page({ target: el, text: 'journal', hidden: true }),
-        title: 'Journal',
-        srcActive: journalIconActive,
-        srcInactive: journalIconInactive,
-      },
-  ]
+  {
+    //index[0]
+    path: '/',
+    //component: Page({ target: el, text: 'Dashboard', hidden: false }),
+    title: 'Dashboard',
+    srcActive: dashboardIconActive,
+    srcInactive: dashboardIconInactive,
+  },
+  {
+    //index[1]
+    path: '/code-buddys',
+    //component: Page({ target: el, text: 'Code Buddys', hidden: true }),
+    title: 'Code Buddys',
+    srcActive: buddyIconActive,
+    srcInactive: buddyIconInactive,
+  },
+  {
+    //index[2]
+    path: '/teams',
+    //component: Page({ target: el, text: 'Teams', hidden: true }),
+    title: 'Teams',
+    srcActive: teamIconActive,
+    srcInactive: teamIconInactive,
+  },
+  {
+    //index[3]
+    path: '/energy',
+    //component: Page({ target: el, text: 'Energy', hidden: true }),
+    title: 'Energy',
+    srcActive: energyIconActive,
+    srcInactive: energyIconInactive,
+  },
+  {
+    //index[4]
+    path: '/journal',
+    //component: Page({ target: el, text: 'journal', hidden: true }),
+    title: 'Journal',
+    srcActive: journalIconActive,
+    srcInactive: journalIconInactive,
+  },
+]
 
+Navigation({ targetSelector: 'footer', onNavigate: handleNavigate, routes })
 
-  Navigation({ target: 'footer', onNavigate: handleNavigate, routes })
+function handleNavigate(path) {
+  /*const newRoute = routes.find((route) => route.path === path)
+                header.update(newRoute.title)
+                routes.forEach((route) =>
+                  route === newRoute ? route.component.show() : route.component.hide()
+                )*/
 
-  function handleNavigate(path) {
-    /*const newRoute = routes.find((route) => route.path === path)
-    header.update(newRoute.title)
-    routes.forEach((route) =>
-      route === newRoute ? route.component.show() : route.component.hide()
-    )*/
+  const pages = getDataJsAll('page')
+  pages.forEach((page) => {
+    const pageName = page.dataset.name
+    const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
+    page.classList.toggle('d-none', targetName !== pageName)
+  })
 
-    const pages = getDataJsAll('page')
-    pages.forEach((page) => {
-        const pageName = page.dataset.name
-        const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
-        page.classList.toggle('d-none', targetName !== pageName)
-      })
+  const headers = getDataJsAll('header')
+  headers.forEach((header) => {
+    const headerName = header.dataset.name
+    const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
+    header.classList.toggle('d-none', targetName !== headerName)
+  })
 
-    const headers = getDataJsAll('header')
-    headers.forEach((header) => {
-        const headerName = header.dataset.name
-        const targetName = path === '/' ? 'dashboard' : path.replace('/', '')
-        header.classList.toggle('d-none', targetName !== headerName)
-      })
-
-
-    console.log('Grüße von der handleNavigate')
-  }
+  //console.log('Grüße von der handleNavigate')
+}
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,67 +1,26 @@
-import {createElement} from './js/lib'
+import { createElement } from './js/lib'
 import './Navigation.css'
 
-
-export default function Navigation({ onNavigate, routes }) {
-    console.log('Navigation')
-    const el = createElement({ type: 'nav', className: 'nav__links', target: document.querySelector('footer') })
-    console.log(el)
-
-    routes.forEach((route, index) => {
-      const btn = createElement({ type: 'button', target: el })
-      const navIcon = createElement( {type: 'img', target: btn })
-      //index === 0 ? navIcon.setAttribute('src', route.srcActive) : navIcon.setAttribute('src', route.srcInactive)
-      navIcon.src = index === 0 ? route.srcActive : route.srcInactive
-      btn.addEventListener('click', () => {
-          onNavigate(route.path)
-          const imgs = el.querySelectorAll('button img')
-          imgs.forEach((img, index) => img.setAttribute('src', routes[index].srcInactive))
-          navIcon.setAttribute('src', route.srcActive)
-        })
-    
-
+export default function Navigation({ onNavigate, routes, targetSelector }) {
+    const el = createElement({
+        type: 'nav',
+        className: 'nav__links',
+        target: document.querySelector(targetSelector),
     })
-  
+    routes.forEach((route, index) => {
+        const btn = createElement({ type: 'button', target: el })
+        const navIcon = createElement({ type: 'img', target: btn })
+            //index === 0 ? navIcon.setAttribute('src', route.srcActive) : navIcon.setAttribute('src', route.srcInactive)
+        navIcon.src = index === 0 ? route.srcActive : route.srcInactive
 
-    return {}
-  }
-
-
-  
-
-
-  /*export default function setupNav() {
-    const navButtons = getDataJsAll('navButton')
-    const pages = getDataJsAll('page')
-    const header = getDataJsAll('header')
-  
-    addNavLogic()
-  
-    function addNavLogic() {
-      navButtons.forEach((navButton) => {
-        navButton.addEventListener('click', (event) => {
-          const clickedButton = event.currentTarget
-          const targetName = clickedButton.dataset.name
-  
-          navButtons.forEach((navButtonItem) => {
-            navButtonItem.firstElementChild.classList.add('d-none')
-            navButtonItem.lastElementChild.classList.remove('d-none')
-          })
-  
-          clickedButton.firstElementChild.classList.remove('d-none')
-          clickedButton.lastElementChild.classList.add('d-none')
-  
-          pages.forEach((page) => {
-            const pageName = page.dataset.name
-            page.classList.toggle('d-none', targetName !== pageName)
-          })
-  
-          header.forEach((header) => {
-            const headerName = header.dataset.name
-            header.classList.toggle('d-none', targetName !== headerName)
-          })
+        btn.addEventListener('click', () => {
+            onNavigate(route.path)
+            const imgs = el.querySelectorAll('button img')
+                //imgs.forEach((img, index) => img.setAttribute('src', routes[index].srcInactive))
+            imgs.forEach((img, index) => (img.src = routes[index].srcInactive))
+                //navIcon.setAttribute('src', route.srcActive)
+            navIcon.src = route.srcActive
         })
-      })
-    }
-  }
-  */
+    })
+    return {}
+}
