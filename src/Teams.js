32507696cb8e14f06b7604550dcca35b4ded5945
neuefische <neuefische@Markus-MacBook-Pro-2.local>
2020-10-23 12:56:18 +0200

    Add Teams.js/.css

---
+++ src/Teams.js
@@ -0,0 +1,18 @@
+import { createElement, createElementPlaceStart } from './js/lib'
+import './Team.css'
+import plus from '/assets/icons/plus.svg'
+
+
+export default function CodeBuddys(pair, main) {
+    console.log(pair)
+    const sectionEl = createElement({ type: 'section', className: 'group mt-24', target: main })
+    const plusEl = createElement({ type: 'img', className: 'group__plus' })
+    const memberElTop = createElement({ className: 'group__member semibold-16--light-blue' })
+    const memberElBottom = createElement({ className: 'group__member semibold-16--light-blue' })
+    plusEl.src = plus
+    sectionEl.appendChild(plusEl)
+    sectionEl.appendChild(memberElBottom)
+    sectionEl.appendChild(memberElTop)
+    memberElTop.textContent = pair[0]
+    memberElBottom.textContent = pair[1]
+}
